f4c4aa1c8d537dc323e0069cbaf99e4a3f80d2e0
pa-tna <60775940+pa-tna@users.noreply.github.com>
2024-01-24 10:54:00 +0000

    Improved usability of transforms * Added helper scripts to prep GATE output for transforms * Added documentation for how to use transforms

--- transforms/postprocess-des-doc.bat
+++ transforms/postprocess-des-doc.bat
@@ -1,16 +1,18 @@
 
 cls
 
-call saxon.bat -s:%1.xml -xsl:xsl/des/markup.xsl -o:%1_des.xml %2 %3
+call saxon.bat -xsl:xsl/prepare-des-document.xsl -it:main "des_legislation_xml=%CD:\=/%/%1_doc.xml" "des_effects_xml=%CD:\=/%/%1_effects.xml" -o:%1_des.xml
 
-call saxon.bat -s:%1_des.xml -xsl:xsl/legislation/fo/legislation_schema_FO.xslt -o:%1_fo.xml  %2 %3
+call saxon.bat -s:%1_des.xml -xsl:xsl/des/markup.xsl -o:%1_des2.xml %2 %3
+
+call saxon.bat -s:%1_des2.xml -xsl:xsl/legislation/fo/legislation_schema_FO.xslt -o:%1_fo.xml  %2 %3
 
 call saxon.bat -s:%1_fo.xml -xsl:xsl/legislation/fo/legislation_schema_cleanup_FO.xslt -o:%1.fo
 
 echo generating %1.pdf
 
-call fop-1.0\fop.bat -c fop/sls.conf -fo %1.fo -pdf %1.pdf
+call fop\fop\fop.bat -c fop/sls.conf -fo %1.fo -pdf %1.pdf
 
 echo generating %1.xls
 
-call saxon.bat -s:%1_des.xml -xsl:xsl/toes-feed-to-extended-worksheet.xsl -o:%1_spreadsheet.xls 
+call saxon.bat -s:%1_des2.xml -xsl:xsl/toes-feed-to-extended-worksheet.xsl -o:%1_spreadsheet.xls 
